29dbe8bc5c13361417540f030bbb953953966c3c
boblovespi <boblovespi3.14@gmail.com>
2025-06-26 12:51:14 +0800

    it outputs asm

--- MeowC/Generators/LLVMGen.cs
+++ MeowC/Generators/LLVMGen.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using LLVMSharp.Interop;
+﻿using LLVMSharp.Interop;
 using MeowC.Interpreter;
 using MeowC.Parser.Matches;
 
@@ -9,10 +8,14 @@ public unsafe class LLVMGen
 {
 	public LLVMGen(List<Definition> definitions)
 	{
+		LLVM.InitializeNativeTarget();
+		LLVM.InitializeNativeAsmParser();
+		LLVM.InitializeNativeAsmPrinter();
 		Definitions = definitions;
 		Context = LLVM.ContextCreate();
 		Builder = LLVM.CreateBuilder();
-		Module = LLVM.ModuleCreateWithName(PinString("meowc"));
+		Module = LLVMModuleRef.CreateWithName("meowc");
+		// Module.DataLayout = LLVM.D
 		Evaluator = new(Context, Builder, Module);
 	}
 
@@ -33,7 +36,13 @@ public unsafe class LLVMGen
 				GenFunctionDef(definition.Id, binop.Left, binop.Right);
 		}
 
-		Console.Write(Module.PrintToString());
+		// Console.Write(Module.PrintToString());
+		Module.Dump();
+		Console.WriteLine();
+		var jit = Module.CreateMCJITCompiler();
+		jit.
+		// Console.Write(Module.);
+		TargetMachine.EmitToFile(Module, "out.a", LLVMCodeGenFileType.LLVMAssemblyFile);
 	}
 
 	private void GenMainDef(string name, Expression body)
@@ -41,7 +50,7 @@ public unsafe class LLVMGen
 		// GenFunctionDef("main", Expression., body);
 	}
 
-	private void GenFunctionDef(string functionName, Expression args, Expression body)
+	public void GenFunctionDef(string functionName, Expression args, Expression body)
 	{
 		int argCount;
 		var argNames = new List<string>();
@@ -57,7 +66,7 @@ public unsafe class LLVMGen
 			default:
 				throw new Exception("Functions require bindings");
 		}
-		
+
 		var llvmargs = new LLVMTypeRef[argCount];
 		llvmargs[0] = LLVMTypeRef.Int32;
 
@@ -66,19 +75,15 @@ public unsafe class LLVMGen
 		var bindings = new Dictionary<IdValue, LLVMValueRef>();
 		for (int i = 0; i < argNames.Count; i++)
 		{
-			var param = LLVM.GetParam(function, (uint)i);
-			LLVM.SetValueName(param, PinString(argNames[i]));
+			var param = function.GetParam((uint)i);
+			param.Name = argNames[i];
 			bindings[new IdValue(argNames[i])] = param;
 		}
 
-		var bb = LLVM.AppendBasicBlock(function, PinString("entry"));
-		LLVM.PositionBuilderAtEnd(Builder, bb);
+		var bb = function.AppendBasicBlock("entry");
+		Builder.PositionAtEnd(bb);
 		var ret = Evaluator.Evaluate(body, bindings);
 		Builder.BuildRet(ret);
-	}
-
-	private sbyte* PinString(string str)
-	{
-		return (sbyte*)Marshal.StringToHGlobalAnsi(str);
+		function.VerifyFunction(LLVMVerifierFailureAction.LLVMPrintMessageAction);
 	}
 }
